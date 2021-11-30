899a5dba813abfc9911b8ebfa26d29ea29e4e8ff
yokoyam daisuke <daisuke.y623@gmail.com>
2021-11-30 20:40:15 +0900

    MENTA用のpusu

---
+++ includes/footer.php
@@ -0,0 +1,65 @@
+<div class="footer__inner">
+  <div class="footer-container">
+    <div class="footer-address">
+      <div class="footer-address__logo">
+        <img src="http://mysite.local/wp-content/themes/WpPractice/img/logo-footer.svg" alt="FOR EDUCATION" class="footer-address__img" />
+        <span class="footer-address__ttl">FOR EDUCATION</span>
+      </div>
+      <address class="footer-address__info">
+        <div class="footer-address__info-item">
+          <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/location_footer.svg" alt="所在地" />
+          <span class="footer-address__info-txt">1500 Treat Ave, Suite 200 San Francisco, CA 94110</span>
+        </div>
+        <div class="footer-address__info-item">
+          <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/mail_footer.svg" alt="メールアドレス" />
+          <span class="footer-address__info-txt">vctung@outlook.com</span>
+        </div>
+        <div class="footer-address__info-item">
+          <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/phone.svg" alt="電話番号" />
+          <span class="footer-address__info-txt">1-800-800-2299 (Support)</span>
+        </div>
+      </address>
+    </div>
+    <nav class="footer-nav">
+      <div class="fotter-nav-item">
+        <span class="fotter-nav-item__ttl">Community</span>
+        <ul>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+        </ul>
+      </div>
+      <div class="fotter-nav-item">
+        <span class="fotter-nav-item__ttl">Community</span>
+        <ul>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+        </ul>
+      </div>
+      <div class="fotter-nav-item">
+        <span class="fotter-nav-item__ttl">Community</span>
+        <ul>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+          <li class="fotter-nav-item__list"><a href="">item</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
+  <div class="copy-right">
+    <span class="copy-right__ttl">Copyright © 2021 Tung Chi Vo</span>
+    <figure>
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Facebook.svg" alt="Facebook" class="copy-right__img" />
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Pinterest.svg" alt="Pinterest" class="copy-right__img" />
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Twitter.svg" alt="Twitter" class="copy-right__img" />
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Instagram.svg" alt="Instagram" class="copy-right__img" />
+    </figure>
+  </div>
+</div>
---
+++ includes/header.php
@@ -0,0 +1,46 @@
+<section class="header-top">
+  <div class="header-top__inner">
+    <address class="address">
+      <figure class="address__inner">
+        <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/location.svg" alt="所在地" class="address__img" /><span class="address__txt">1600 Amphitheatre Parkway, CA 94043</span>
+      </figure>
+      <figure class="address__inner">
+        <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/mail.svg" alt="メールアドレス" class="address__img" /><span class="address__txt">vctung@outlook.com</span>
+      </figure>
+    </address>
+    <figure class="sns-items">
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Facebook.svg" alt="Facebook" class="sns-items__img" />
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Pinterest.svg" alt="Pinterest" class="sns-items__img" />
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Twitter.svg" alt="Twitter" class="sns-items__img" />
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/Instagram.svg" alt="Instagram" class="sns-items__img" />
+    </figure>
+  </div>
+</section>
+<section class="header-bottom">
+  <div class="header-bottom__inner">
+    <div class="logo">
+      <img src="http://mysite.local/wp-content/themes/WpPractice/img/logo.svg" alt="FOR EDUCATION" class="logo__img" />
+      <h1 class="logo__h1">FOR EDUCATION</h1>
+    </div>
+    <nav class="nav">
+      <ul class="nav__inner">
+        <li class="nav__item">
+          <a href=""><span class="nav-items__link">Home</span></a>
+        </li>
+        <li class="nav__item">
+          <a href=""><span>About</span></a>
+        </li>
+        <li class="nav__item">
+          <a href=""><span>Courses</span></a>
+        </li>
+        <li class="nav__item">
+          <a href=""><span>Contact</span></a>
+        </li>
+      </ul>
+      <a class="btn is-primary pt-1 pb-1 pr-2 pl-2 is-size-1_5">Sign up</a>
+      <div class="search">
+        <img src="http://mysite.local/wp-content/themes/WpPractice/img/icon/search.svg" alt="サイト内検索" />
+      </div>
+    </nav>
+  </div>
+</section>
